c0071b9799f952e3f1b6bf46a2559806ff355faf
Mi Guoliang <boymgl@qq.com>
2025-12-13 16:38:01 +0800

    chore: remove temporary sanity check test

--- __tests__/utils/math.test.ts
+++
@@ -1,9 +0,0 @@
-describe('Math Utils', () => {
-  it('should pass a basic truth test', () => {
-    expect(true).toBe(true);
-  });
-
-  it('should calculate 1 + 1 correctly', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
